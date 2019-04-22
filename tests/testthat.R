01b5b0e553aa4eb034ba476ba5b81941f702df8b
Landon Sego <LHS@byu.net>
2019-04-22 05:54:29 +0000

    version 0.3.9.1

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,6 +6,7 @@ Sys.setenv("R_TESTS" = "")
 
 library(testthat)
 library(Smisc)
+suppressWarnings(RNGversion("3.5.0"))
 
 # There shouldn't be any files in the tmp folder
 unlink("testthat/tmp", recursive = TRUE, force = TRUE)
